15560929047c5bfe3b5e1135406367af101a2b9a
blackstrype <blackstrype@gmail.com>
2025-11-03 10:41:41 +0100

    cartKata CartPriceCalculator.feature scenario - max client pro - refactor introduce constants

--- src/main/java/cart/CartPriceCalculator.java
+++ src/main/java/cart/CartPriceCalculator.java
@@ -4,7 +4,12 @@ import cart.model.Cart;
 import cart.model.Client;
 
 public class CartPriceCalculator {
+
+    public static final int UNIT_PRICE_PRO_ELITE_HIGH_END_TELEPHONE = 1000;
+    public static final int UNIT_PRICE_PRO_ELITE_MIDDLE_END_TELEPHONE = 550;
+
     public static int calculateTotalPrice(Cart cart, Client client) {
-        return cart.getHighEndTelephoneCount() * 1000 + cart.getMiddleEndTelephoneCount() * 550;
+        return cart.getHighEndTelephoneCount() * UNIT_PRICE_PRO_ELITE_HIGH_END_TELEPHONE
+                + cart.getMiddleEndTelephoneCount() * UNIT_PRICE_PRO_ELITE_MIDDLE_END_TELEPHONE;
     }
 }
