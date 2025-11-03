364fd0c01f1bf69bdf3ef7529eca5bf150796ed9
blackstrype <blackstrype@gmail.com>
2025-11-03 10:54:44 +0100

    cartKata CartPriceCalculator.feature scenario - small client pro - scenario template complete with implementation.

--- src/main/java/cart/CartPriceCalculator.java
+++ src/main/java/cart/CartPriceCalculator.java
@@ -8,10 +8,19 @@ public class CartPriceCalculator {
     public static final int UNIT_PRICE_PRO_ELITE_HIGH_END_TELEPHONE = 1000;
     public static final int UNIT_PRICE_PRO_ELITE_MIDDLE_END_TELEPHONE = 550;
     private static final int UNIT_PRICE_PRO_ELITE_LAPTOP = 900;
+    private static final int UNIT_PRICE_PRO_HIGH_END_TELEPHONE = 1150;
+    private static final int UNIT_PRICE_PRO_MIDDLE_END_TELEPHONE = 600;
+    private static final int UNIT_PRICE_PRO_LAPTOP = 1000;
 
     public static int calculateTotalPrice(Cart cart, Client client) {
-        return cart.getHighEndTelephoneCount() * UNIT_PRICE_PRO_ELITE_HIGH_END_TELEPHONE
-                + cart.getMiddleEndTelephoneCount() * UNIT_PRICE_PRO_ELITE_MIDDLE_END_TELEPHONE
-                + cart.getLaptopCount() * UNIT_PRICE_PRO_ELITE_LAPTOP;
+        if (client.getTurnover() > 10000000) {
+            return cart.getHighEndTelephoneCount() * UNIT_PRICE_PRO_ELITE_HIGH_END_TELEPHONE
+                    + cart.getMiddleEndTelephoneCount() * UNIT_PRICE_PRO_ELITE_MIDDLE_END_TELEPHONE
+                    + cart.getLaptopCount() * UNIT_PRICE_PRO_ELITE_LAPTOP;
+        } else {
+            return cart.getHighEndTelephoneCount() * UNIT_PRICE_PRO_HIGH_END_TELEPHONE
+                    + cart.getMiddleEndTelephoneCount() * UNIT_PRICE_PRO_MIDDLE_END_TELEPHONE
+                    + cart.getLaptopCount() * UNIT_PRICE_PRO_LAPTOP;
+        }
     }
 }
--- src/test/resources/cart/CartPriceCalculator.feature
+++ src/test/resources/cart/CartPriceCalculator.feature
@@ -18,3 +18,22 @@ Feature: Calculate the total price of the cart of telephone items for an identif
       | 0    | 0      | 1      | 900   |
       | 0    | 0      | 2      | 1800  |
       | 10   | 10     | 10     | 24500 |
+
+  Scenario Template: Professional clients with less than 10 million in turnover
+    Given the client is a Professional
+    And the Professional client's turnover is 9999999
+    And there are <high> high-end telephones in the shopping cart
+    And there are <middle> middle-end telephones in the shopping cart
+    And there are <laptop> laptops in the shopping cart
+    When the price of the cart is calculated
+    Then the total price of their cart will be <total> euros
+    Examples:
+      | high | middle | laptop | total |
+      | 0    | 0      | 0      | 0     |
+      | 1    | 0      | 0      | 1150  |
+      | 2    | 0      | 0      | 2300  |
+      | 0    | 1      | 0      | 600   |
+      | 0    | 2      | 0      | 1200  |
+      | 0    | 0      | 1      | 1000  |
+      | 0    | 0      | 2      | 2000  |
+      | 10   | 10     | 10     | 27500 |
